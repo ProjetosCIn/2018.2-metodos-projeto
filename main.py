920cc6dca7ad9de214706dba83710fb4774a891b
Thiago Augusto <martinsthiagoaugusto@gmail.com>
2018-10-05 08:41:04 -0300

    Adicionada solução euler aprimorado

--- main.py
+++ main.py
@@ -2,16 +2,39 @@ from sympy import sympify
 
 def euler(y0, t0, h, qntSteps, func):
 	expr = sympify(func)
+	print("Metodo de Euler")
 	print(expr)
 	print("y(", t0, ") = ", y0)
 	print("h = ", h)
 	lastY = y0
-	for step in range (0, qntSteps):
+	currentY = lastY
+	for step in range (0, qntSteps + 1):
+		print(int(step), " ", currentY)
 		currentY = lastY + (expr.subs([("y", float(lastY)), ("t", t0)]))*h
 		lastY = float(currentY)
 		t0 += h
+	print("")
+	return 0
 
-		print(int(step + 1), " ", currentY)
+def euler_aprimorado(y0, t0, h, qntSteps, func):
+	expr = sympify(func)
+	print("Metodo de Euler Aprimorado")
+	print(expr)
+	print("y(", t0, ") = ", y0)
+	print("h = ", h)
+	lastY = y0
+	currentY = lastY
+	for step in range (0, qntSteps + 1):
+		print(step, " ", currentY)
+
+		fn = expr.subs([("t", t0) , ("y", float(lastY))])
+		fn_1 = expr.subs([("t", t0 + 1), ("y", float(fn))])
+
+		currentY = lastY + ((fn + fn_1) * h) / 2
+		lastY = float(currentY)
+		t0 += h
+		
+	print("")
 	return 0
 
 
@@ -27,6 +50,6 @@ def readFile(path):
 			elif(inputs[0] == 'euler_inverso'):
 				pass
 			elif(inputs[0] == 'euler_aprimorado'):
-				pass
+				euler_aprimorado(float(inputs[1]), float(inputs[2]), float(inputs[3]), int(inputs[4]), inputs[5])
 
 main()
