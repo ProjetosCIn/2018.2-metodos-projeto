4318723ea766eebfccbb961b72b324a60de6cb3a
Thiago Augusto <martinsthiagoaugusto@gmail.com>
2018-10-17 08:21:04 -0300

    Metodo de Runge-Kutta adicionado

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # 2018.2-metodos-projeto
 Projeto final da cadeira métodos numéricos.
+
+
+## Como rodar o programa
+Em uma máquina Linux execute os seguintes comandos no terminal, estando na pasta raiz.
+
+`sudo ./requirements & source RUNME & python main.py`
--- main.py
+++ main.py
@@ -37,6 +37,30 @@ def euler_aprimorado(y0, t0, h, qntSteps, func):
 	print("")
 	return 0
 
+def runge_kutta(y0, t0, h, qntSteps, func):
+	expr = sympify(func)
+	euler = sympify('yn + h * func')
+
+	print("Metodo de Runge-Kutta")
+	print(expr)
+	print("y(", t0, ") = ", y0)
+	print("h = ", h)
+	lastY = y0
+	currentY = lastY
+
+	for step in range (0, qntSteps + 1):
+		print(step, " ", currentY)
+		
+		k1 = expr.subs([("t", t0) , ("y", float(lastY))])
+		k2 = expr.subs([("t", t0 + h / 2) , ("y", float(lastY) + h / 2 * k1)])
+		k3 = expr.subs([("t", t0 + h / 2) , ("y", float(lastY) + h / 2 * k2)])
+		k4 = expr.subs([("t", t0 + h) , ("y", float(lastY) + h * k3)])
+
+		currentY = lastY + h / 6 * (k1 + 2 * k2 + 2 * k3 + k4)
+		lastY = float(currentY)
+		t0 += h
+	print("")	
+	return 0
 
 def main():
 	readFile('entradas.txt')
@@ -51,5 +75,7 @@ def readFile(path):
 				pass
 			elif(inputs[0] == 'euler_aprimorado'):
 				euler_aprimorado(float(inputs[1]), float(inputs[2]), float(inputs[3]), int(inputs[4]), inputs[5])
+			elif(inputs[0] == 'runge_kutta'):
+				runge_kutta(float(inputs[1]), float(inputs[2]), float(inputs[3]), int(inputs[4]), inputs[5])
 
 main()
